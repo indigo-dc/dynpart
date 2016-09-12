915ee5bc42e96439ea69cdbc3eea9c54022d2ae6
Sonia Taneja <sonia.taneja@t1-cloudcc-02.cr.cnaf.infn.it>
2016-09-12 18:00:46 +0200

    test added

--- bin/submitter_demo.py
+++ bin/submitter_demo.py
@@ -7,8 +7,18 @@ import random
 import commands
 import json
 
-# queste cose da mettere in file conf
-
+"""Copyright (c) 2015 INFN - INDIGO-DataCloud
+All Rights Reserved
+Licensed under the Apache License, Version 2.0;
+you may not use this file except in compliance with the
+License. You may obtain a copy of the License at:
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied.
+See the License for the specific language governing
+permissions and limitations under the License."""
 
 conf_file = '/etc/indigo/dynpart/dynp.conf'
 
